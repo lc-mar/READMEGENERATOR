a8fbacc89c0636376fa896eb6cdb2cb59dd8d3a6
lc-mar <78833615+lc-mar@users.noreply.github.com>
2021-09-17 18:14:33 -0400

    organized file structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   switch(license) {
     case 'Apache': return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
@@ -12,20 +11,20 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   let badge = renderLicenseBadge(data.license)
   return `# ${data.title}
   ${badge}
 
+  Table of Contents |
+  ----------------- |
+  [Description](#description) |
+  [Installation](#installation) |
+  [Usage](#usage) |
+  [Contributors](#contribution) |
+  [Tests](#tests) |
+  [License](#license) |
+  [Questions](#questions) |
 `;
 }
 
