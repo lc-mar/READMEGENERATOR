68c581d0033b02872fab9e7f0bcb05b4c36d62c3
lc-mar <78833615+lc-mar@users.noreply.github.com>
2021-09-17 18:03:36 -0400

    updated questions and packages in index.jsx

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter Title of Project:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter Description of Project:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter Installation Instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter Usage Information:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter Contributors:',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Enter Test Instructions',
+        name: 'tests',
+    },
+    {
+        type: 'list',
+        message: 'Select a License:',
+        name: 'license',
+        choices: ['Apache', 'BSD', 'GNU', 'IBM', 'MIT', 'Mozilla'],
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
