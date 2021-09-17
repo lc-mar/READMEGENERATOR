62a57f851483656ec863fb5c84ce5fd556227fed
lc-mar <78833615+lc-mar@users.noreply.github.com>
2021-09-17 17:51:41 -0400

    added functions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -4,10 +4,18 @@
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+let markdownText = generateMarkdown(data)
+    fs.writeFile(fileName, markdownText, (err) =>
+        err ? console.log(err): console.log('README created'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+inquirer.prompt(questions).then(data => {
+    const fileName = `${data.title.toLowerCase().split(' ').join('')}.md`;
+    writeToFile(fileName, data);
+});
+}
 // Function call to initialize app
 init();
