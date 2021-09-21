7c2d1a3b3f330d6a75ea89f3ac2809176a8da7d1
lc-mar <78833615+lc-mar@users.noreply.github.com>
2021-09-21 14:54:48 -0400

    updated

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// TODO: Include packages needed for this application
+function renderLicenseBadge(license) {
+    switch(license) {
+      case 'Apache': return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      case 'BSD': return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      case 'GNU': return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'IBM': return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+      case 'MIT': return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Mozilla': return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      default: return '';
+    }
+  }
+  
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const markdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -54,19 +64,17 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 let markdownText = generateMarkdown(data)
     fs.writeFile(fileName, markdownText, (err) =>
         err ? console.log(err): console.log('README created'))
 }
 
-// TODO: Create a function to initialize app
 function init() {
 inquirer.prompt(questions).then(data => {
     const fileName = `${data.title.toLowerCase().split(' ').join('')}.md`;
     writeToFile(fileName, data);
 });
 }
-// Function call to initialize app
+
 init();
